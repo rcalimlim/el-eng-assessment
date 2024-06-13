a57acfd576912d76e7302ac49d784c3113fd480e
Ross F. Calimlim <ross.calimlim@gmail.com>
2024-06-13 18:39:13 -0400

    add local data fixture and adapter layer

--- db/json-data-adapter.ts
+++ db/json-data-adapter.ts
@@ -1,50 +1,55 @@
+import { v4 as uuidv4 } from 'uuid';
 import * as fs from 'fs';
 import { DataProviderAdapter } from './data-provider-adapter';
+import { CreateFoodtruckDto } from 'src/foodtrucks/dto/create-foodtruck.dto';
+import { UpdateFoodtruckDto } from 'src/foodtrucks/dto/update-foodtruck.dto';
 
 const JSON_FILE_PATH = './fixtures.json';
 
 export class JsonAdapter implements DataProviderAdapter {
-  private readData() {
-    const data = fs.readFileSync(JSON_FILE_PATH, 'utf8');
-    return JSON.parse(data);
+  private read() {
+    try {
+      const data = fs.readFileSync(JSON_FILE_PATH, 'utf8');
+      return JSON.parse(data);
+    } catch (e) {
+      console.error(e);
+      throw new Error('data connection failed');
+    }
   }
 
-  private writeData(data: any) {
+  private write(data: any) {
     fs.writeFileSync(JSON_FILE_PATH, JSON.stringify(data, null, 2));
   }
 
-  async getAll() {
-    return this.readData();
+  public async getAll() {
+    return this.read();
   }
 
-  async getById(id: number) {
-    const data = this.readData();
+  public async getById(id: number) {
+    const data = this.read();
     return data.find((item: any) => item.id === id);
   }
 
-  async create(data: any) {
-    const currentData = this.readData();
-    // const newId = currentData.length
-    //   ? Math.max(...currentData.map((item: any) => item.id)) + 1
-    //   : 1;
-    const newData = { id: newId, ...data };
+  public async create(data: CreateFoodtruckDto) {
+    const currentData = this.read();
+    const newData = { id: uuidv4(), ...data };
     currentData.push(newData);
-    this.writeData(currentData);
+    this.write(currentData);
     return newData;
   }
 
-  async update(id: number, data: any) {
-    const currentData = this.readData();
+  public async update(id: number, data: UpdateFoodtruckDto) {
+    const currentData = this.read();
     const index = currentData.findIndex((item: any) => item.id === id);
     if (index === -1) throw new Error('Item not found');
     currentData[index] = { id, ...data };
-    this.writeData(currentData);
+    this.write(currentData);
     return currentData[index];
   }
 
-  async delete(id: number) {
-    const currentData = this.readData();
+  public async delete(id: number) {
+    const currentData = this.read();
     const newData = currentData.filter((item: any) => item.id !== id);
-    this.writeData(newData);
+    this.write(newData);
   }
 }
